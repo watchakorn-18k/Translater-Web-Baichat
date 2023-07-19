86c7a756dd916217c2b63030a3dc3161571ea050
watchakorn-18k <porton2559@gmail.com>
2023-07-19 18:04:04 +0700

    Build to github

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Translater-Web-Baichat/",
+  base: "/Translator-with-GPT/",
 });
